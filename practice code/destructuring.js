428bf15b36326cde10f595e532d585ed751010df
ankit <io6.ankitmaurya@gmail.com>
2024-06-01 21:12:40 +0530

    destructuring

---
+++ practice code/destructuring.js
@@ -0,0 +1,259 @@
+const {
+  info: {
+    slugs: { restaurant },
+    info: { name, category },
+  },
+} = {
+  info: {
+    id: "209675",
+    name: "S K Food Plaza",
+    city: "Ludhiana",
+    slugs: {
+      restaurant: "s-k-food-plaza-chaura-bazar-chaura-bazar",
+      city: "ludhiana",
+    },
+    uniqueId: "e51e6528-4859-48c8-8fed-e84c17dc2d76",
+    cloudinaryImageId: "hpwvad9uatvfszrixvoo",
+    locality: "New Shiv Puri",
+    areaName: "Ludhiana Junction",
+    costForTwo: "25000",
+    costForTwoMessage: "₹250 for two",
+    cuisines: ["Chinese", "Fast Food"],
+    avgRating: 4.4,
+    feeDetails: {
+      restaurantId: "209675",
+      fees: [
+        {
+          name: "TYPE_DISTANCE",
+          fee: 1200,
+        },
+        {
+          name: "TYPE_TIME",
+        },
+      ],
+      totalFee: 1200,
+      title: "Delivery Charge",
+      amount: "1200",
+      icon: "v1648635511/Delivery_fee_new_cjxumu",
+      message: "Order above 149 for discounted delivery fee",
+    },
+    parentId: "173449",
+    avgRatingString: "4.4",
+    totalRatingsString: "1K+ ratings",
+    sla: {
+      restaurantId: "209675",
+      deliveryTime: 31,
+      minDeliveryTime: 30,
+      maxDeliveryTime: 35,
+      lastMileTravel: 4.4,
+      serviceability: "SERVICEABLE",
+      stressFactor: 1,
+      rainMode: "RAIN_MODE_NONE",
+      longDistance: "LONG_DISTANCE_NOT_LONG_DISTANCE",
+      zoneId: 852,
+      slaString: "30-35 MINS",
+      lastMileTravelString: "4.4 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2024-05-25 22:50:00",
+      visibility: true,
+      opened: true,
+      restaurantClosedMeta: {},
+    },
+    aggregatedDiscountInfo: {
+      header: "Buy 1 Get 1 Free",
+      shortDescriptionList: [
+        {
+          meta: "Buy 1, Get 1 Free | Use BOGO4IPL",
+          discountType: "BXGY",
+          operationType: "RESTAURANT",
+        },
+        {
+          meta: "40% off | Use TRYNEW",
+          discountType: "Percentage",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "Buy 1, Get 1 Free | Use BOGO4IPL",
+          discountType: "BXGY",
+          operationType: "RESTAURANT",
+        },
+        {
+          meta: "40% off up to ₹80 | Use code TRYNEW",
+          discountType: "Percentage",
+          operationType: "RESTAURANT",
+        },
+      ],
+      visible: true,
+    },
+    badges: {},
+    slugString: "s-k-food-plaza-chaura-bazar-chaura-bazar",
+    isOpen: true,
+    labels: [
+      {
+        title: "Timings",
+        message: "null",
+      },
+      {
+        title: "Address",
+        message: "New Shiv Puri, Ludhiana, Punjab, India",
+      },
+      {
+        title: "Cuisines",
+        message: "Chinese,Fast Food",
+      },
+    ],
+    totalRatings: 1000,
+    aggregatedDiscountInfoV2: {
+      header: "Buy 1 Get 1 Free",
+      shortDescriptionList: [
+        {
+          meta: "Buy 1, Get 1 Free | Use BOGO4IPL",
+          discountType: "BXGY",
+          operationType: "RESTAURANT",
+        },
+        {
+          meta: "40% off | Use TRYNEW",
+          discountType: "Percentage",
+          operationType: "RESTAURANT",
+        },
+      ],
+      descriptionList: [
+        {
+          meta: "Buy 1, Get 1 Free | Use BOGO4IPL",
+          discountType: "BXGY",
+          operationType: "RESTAURANT",
+        },
+        {
+          meta: "40% off up to ₹80 | Use code TRYNEW",
+          discountType: "Percentage",
+          operationType: "RESTAURANT",
+        },
+      ],
+      couponDetailsCta: "View coupon details",
+    },
+    type: "F",
+    nudgeBanners: [
+      {
+        priority: 1,
+        couponCode: "BOGO4IPL",
+        discountInfo: {
+          discountType: "BXGY",
+          value: 100,
+        },
+        lockedMessage: "Add <amount> more <TagInfo> item(s) to avail the offer",
+        unlockedMessage: "Yay! 1 item worth <X> is now FREE!",
+        minItemCount: 1,
+        maxItemCount: 2,
+        type: "B1G1",
+        nudgeTagInfo: {
+          title: "Buy 1 Get 1",
+          textColor: "#ffffff",
+          backgroundColor: "#753428",
+          separatorColor: "#E44629",
+          fontName: "FONT_NAME_CONDENSED_BOLD",
+        },
+        styling: {
+          backgroundColor: "#2C1F1D",
+          textColor: "#B3FFFFFF",
+        },
+        logoCtx: {},
+      },
+    ],
+    headerBanner: {
+      url: "swiggy://webview?is_external=false&webview_url=https://www.swiggy.com/restaurant-info/209675",
+    },
+    expectationNotifiers: [
+      {
+        text: "Order above 149 for discounted delivery fee",
+        icon: {
+          imageId: "v1648635511/Delivery_fee_new_cjxumu",
+        },
+        popup: {
+          cta: {},
+        },
+        type: "DISTANCE_FEE_NON_FOOD_LM",
+        enrichedText: "Order above 149 for discounted delivery fee",
+        halfCardPopup: {
+          halfCardPopupHeader: {},
+        },
+        trailingIcon: {},
+      },
+    ],
+    ratingSlab: "RATING_SLAB_5",
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    hasBestsellerItems: true,
+    cartOrderabilityNudgeBanner: {
+      parameters: {},
+      presentation: {},
+    },
+    latLong: "30.930935,75.85712899999999",
+    backgroundImageOverlayInfo: {},
+    info: {
+      id: "43546773",
+      name: "Cheese Chilli",
+      category: "Chinese",
+      description:
+        "Serves 1 | Spicy chilli is spooned over, and topped with, Shredded cheese for a bit of cheese in every paneer bite.",
+      imageId: "p2srgpssaq3s6qcknwqb",
+      inStock: 1,
+      isVeg: 1,
+      price: 22400,
+      variants: {
+        variantGroups: [
+          {
+            groupId: "4984811",
+            name: "Quantity",
+            variations: [
+              {
+                name: "Half",
+                default: 1,
+                id: "16290378",
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+              {
+                name: "Full",
+                price: 19200,
+                id: "16290379",
+                inStock: 1,
+                isVeg: 1,
+                isEnabled: 1,
+              },
+            ],
+          },
+        ],
+      },
+      variantsV2: {},
+      itemAttribute: {
+        vegClassifier: "VEG",
+        spiceLevel: "MEDIUMSPICY",
+        portionSize: "Serves 1",
+      },
+      ribbon: {},
+      showImage: true,
+      itemBadge: {},
+      badgesV2: {},
+      ratings: {
+        aggregatedRating: {
+          rating: "4.0",
+          ratingCount: "34 ratings",
+          ratingCountV2: "34",
+        },
+      },
+    },
+  },
+};
+document.write("Hi my name is ankit maurya");
+document.write("<br/>I went to ", restaurant, "  restro and i ordered ", name);
+document.write("<br/>Which is type of ", category);
+// document.write("It was ", name, "   plate.");
