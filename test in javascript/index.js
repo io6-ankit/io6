91a0acf3175cb53f0bfd4b67d8a87f336289e02f
io6-ankit <156927710+io6-ankit@users.noreply.github.com>
2025-02-13 16:15:23 +0530

    code

---
+++ test in javascript/index.js
@@ -0,0 +1,21 @@
+// var arr = [1, 2, 1, 3, 1, 3, 4, 5, 6];
+// arr = valueDuplicates(arr);
+// function valueDuplicates(arr) {
+//   var result = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     var foundItem = false;
+//     for (j = 0; j < result.length; j++) {
+//       if (arr[i] == result[j]) {
+//         foundItem = true;
+//         break;
+//       }
+//     }
+//     if (!foundItem) {
+//       result[result.length] = arr[i];
+//     }
+//   }
+// }10
+10 + "20";
+console.log(10 + "20");
+var num = NaN;
+console.log(num);
